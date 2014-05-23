8a8ef8894a35c976ed4fc72b5adfff700f856b5a
JimboSinister <jimibort@gmail.com>
2014-05-23 11:39:23 -0500

    completed R code for assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,6 @@
-## Put comments here that give an overall description of what your
-## functions do
 ## These two functions are designed to speed the computation of matrix inversion
 ## This is done by caching the the computed inversion rather than computing it repeatedly
 
-## Write a short comment describing this function
 ## Creates a special matrix object that can cashe its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
@@ -20,15 +17,13 @@ makeCacheMatrix <- function(x = matrix()) {
 		getSolve = getSolve)
 }
 
-
-## Write a short comment describing this function
 ## Computes the inverse of the special matrix returned by makeCacheMatrix above
 
 cacheSolve <- function(x, ...) {
       ## Return a matrix that is the inverse of 'x'
 	m <- x$getsolve()
 	if(!is.null(m)) {
-		message("getting cached data")
+		message("getting cached matrix")
 		return(m)
 	}
 	data <- x$get()
